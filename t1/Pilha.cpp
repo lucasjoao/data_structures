63df74f1853f7528549f0e72b009c7d21498bac0
lucasjoao <lucasjoao.lj@gmail.com>
2016-03-30 22:30:36 -0300

    add estrutura básica do t1

---
+++ t1/Pilha.cpp
@@ -0,0 +1,76 @@
+/* Copyright [2016] <Lucas João Martins>
+ * Pilha.cpp
+ */
+
+ #include <iostream>
+ #include "Pilha.hpp"
+
+/*
+ *
+ */
+ class Pilha {
+
+ 	/*
+ 	 *
+ 	 */
+ 	Pilha(){
+
+ 	}
+
+ 	/*
+ 	 *
+ 	 */
+ 	Pilha<T>(int t){
+
+ 	}
+
+ 	/*
+ 	 *
+ 	 */
+ 	void empilha(T dado){
+
+ 	}
+
+ 	/*
+ 	 *
+ 	 */
+ 	T desempilha(){
+
+ 	}
+
+ 	/*
+ 	 *
+ 	 */
+ 	T topo(){
+
+ 	}
+
+
+ 	/*
+ 	 *
+ 	 */
+ 	int getPosTopo(){
+
+ 	}
+
+ 	/*
+ 	 *
+ 	 */
+ 	void limparPilha(){
+
+ 	}
+
+ 	/*
+ 	 *
+ 	 */
+ 	bool PilhaVazia(){
+
+ 	}
+
+ 	/*
+ 	 *
+ 	 */
+ 	bool PilhaCheia(){
+
+ 	}
+ }
