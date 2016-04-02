7057b703f52df7926834a11a25602d25ea0ada9e
lucasjoao <lucasjoao.lj@gmail.com>
2016-04-02 14:19:52 -0300

    primeiros passos corretos no t1

--- t1/Pilha.hpp
+++ t1/Pilha.hpp
@@ -1,15 +1,29 @@
+// Copyright [2016] - Lucas João Martins
+
 template<typename T>
 class Pilha {
-private:
 
-public:
-		Pilha()
-		Pilha<T>(int t)
-	void empilha(T dado)
-	T desempilha()
-	T topo()
-	int getPosTopo()
-	void limparPilha()
-	bool PilhaVazia()
-	bool PilhaCheia()
+	private:
+		T* dados;
+		int topo;
+
+	public:
+
+		Pilha() {}
+
+		Pilha<T>(int t);
+
+		void empilha(T dado); // slide 11
+
+		T desempilha(); // slide 12
+
+		T topo(); // slide 13
+
+		int getPosTopo();
+
+		void limparPilha(); // slide 8
+
+		bool PilhaVazia(); // slide 10
+
+		bool PilhaCheia(); // slide 9
 };
