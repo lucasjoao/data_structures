d000299d0d513e302ad4ddaa190fd6d2a76680d7
lucasjoao <lucasjoao.lj@gmail.com>
2016-04-07 07:53:18 -0300

    copy declarations of moodle

---
+++ t3_lista_de_vetor/Lista.hpp
@@ -0,0 +1,73 @@
+template<typename T>  // nao sei se entra, ja que n esta no vpl
+class Lista {
+	private:
+
+	public:
+		Lista() {
+
+		}
+
+		explicit Lista(int tam) {
+
+		}
+
+		void adiciona(T dado) {
+
+		}
+
+		void adicionaNoInicio(T dado) {
+
+		}
+
+		void adicionaNaPosicao(T dado, int posicao) {
+
+		}
+
+		void adicionaEmOrdem(T dado) {
+
+		}
+
+		T retira() {
+
+		}
+
+		T retiraDoInicio() {
+
+		}
+
+		T retiraDaPosicao(int posicao) {
+
+		}
+
+		int posicao(T dado) {
+
+		}
+
+		bool contem(T dado) {
+
+		}
+
+		bool igual(T dado1, T dado2) {
+
+		}
+
+		bool maior(T dado1, T dado2) {
+
+		}
+
+		bool menor(T dado1, T dado2) {
+
+		}
+
+		bool listaCheia() {
+
+		}
+
+		bool listaVazia() {
+
+		}
+
+		void destroiLista() {
+
+		}
+};
