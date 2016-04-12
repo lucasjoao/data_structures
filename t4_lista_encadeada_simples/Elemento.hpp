503a46ed8ebbee61c5ba3d82806fb69ecf2feced
lucasjoao <lucasjoao.lj@gmail.com>
2016-04-12 07:57:47 -0300

    t4, add all declarations and test file

---
+++ t4_lista_encadeada_simples/Elemento.hpp
@@ -0,0 +1,28 @@
+#ifndef ELEMENTO_HPP
+#define ELEMENTO_HPP
+
+template<typename T>
+class Elemento {
+	public:
+		Elemento(const T &info, Elemento<T> *next) : info(new T(info)), _next(next) {
+
+		}
+
+		~Elemento() {
+			delete info;
+		}
+
+		Elemento<T> *getProximo() const {
+			return _next;
+		}
+
+		T getInfo() const {
+			return *info;
+		}
+
+		void setProximo(Elemento<T> *next) {
+			_next = next;
+		}
+};
+
+#endif
