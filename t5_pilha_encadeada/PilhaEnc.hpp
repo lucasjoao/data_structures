33246ba3a630c485c5a4017c6d6039bfde45a920
lucasjoao <lucasjoao.lj@gmail.com>
2016-04-21 17:49:34 -0300

    t5, add start files

---
+++ t5_pilha_encadeada/PilhaEnc.hpp
@@ -0,0 +1,16 @@
+/**
+ *  Copyright [2016] - Lucas Joao Martins
+ *  PilhaEnc.hpp
+ */
+
+template<typename T>
+class PilhaEnc{
+    public:
+        PilhaEnc()
+        ~PilhaEnc()
+        void empilha(const T& dado)
+        T desempilha()
+    	T topo()
+    	void limparPilha()
+     	bool PilhaVazia()
+};
