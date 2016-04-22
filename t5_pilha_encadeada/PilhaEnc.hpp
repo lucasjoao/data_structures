c8607e71d3f97b9305835e869b1f3b638ad208f5
lucasjoao <lucasjoao.lj@gmail.com>
2016-04-21 23:47:59 -0300

    t5, done and pass in vpl with 10

--- t5_pilha_encadeada/PilhaEnc.hpp
+++ t5_pilha_encadeada/PilhaEnc.hpp
@@ -6,49 +6,34 @@
 #include "ListaEnc.hpp"
 
 template<typename T>
-class PilhaEnc : public ListaEnc<T>{
-    public:
-        PilhaEnc() : ListaEnc<T>::ListaEnc() {}
+class PilhaEnc : ListaEnc<T> {
+	public:
+		PilhaEnc() : ListaEnc<T>::ListaEnc() {}
 
-        ~PilhaEnc() {
-            limparPilha();
-        }
+		~PilhaEnc() {
+			limparPilha();
+		}
 
-        void empilha(const T& dado) {
-            ListaEnc<T>::adicionaNoInicio(dado);
-        }
+		void empilha(const T &dado) {
+			ListaEnc<T>::adicionaNoInicio(dado);
+		}
 
-        T desempilha() {
-            return ListaEnc<T>::retiraDoInicio();
-        }
+	T desempilha() {
+		return ListaEnc<T>::retiraDoInicio();
+	}
 
-    	T topo() {
-            if (PilhaVazia()) {
-                throw "problema";
-            } else {
-                Elemento<T> *tmpElemento = ListaEnc<T>::getHead();
+	T topo() {
+		if (PilhaVazia())
+			throw "problema";
+		else
+			return ListaEnc<T>::getHead()->getInfo();
+	}
 
-                /*for (int i = 0; i < ListaEnc<T>::getSize(); i++)
-                    tmpElemento = tmpElemento->getProximo();*/
+	void limparPilha() {
+		ListaEnc<T>::destroiLista();
+	}
 
-                /*while (tmpElemento != nullptr) {
-                    if (tmpElemento->getProximo() == nullptr)
-                        return tmpElemento->getInfo();
-
-                    tmpElemento = tmpElemento->getProximo();
-                }*/
-
-                // return tmpElemento->getInfo();
-
-                // return T(NULL);
-            }
-        }
-
-    	void limparPilha() {
-            ListaEnc<T>::destroiLista();
-        }
-
-     	bool PilhaVazia() {
-            return ListaEnc<T>::listaVazia();
-        }
+	bool PilhaVazia() {
+		return ListaEnc<T>::listaVazia();
+	}
 };
