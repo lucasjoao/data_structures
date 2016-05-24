bbb2cababa4f9b0fe6007b18c9298ab38bf34ea8
lucasjoao <lucasjoao.lj@gmail.com>
2016-05-24 09:54:36 -0300

    t11, write some doc

--- t11_arvore_binaria_busca/NoBinario.hpp
+++ t11_arvore_binaria_busca/NoBinario.hpp
@@ -1,89 +1,166 @@
 #ifndef NO_BINARIO_HPP
 #define NO_BINARIO_HPP
 
+/*!
+ *  \brief NoBinario.hpp
+ *	\copyright Copyright [2016], Lucas Joao Martins
+ *  \author Lucas Joao Martins
+ *
+ *	Implementa arvore binaria de busca com percurso (bst) atraves de template.
+ */
+
 #include <cstdlib>
 #include <vector>
 
 template<typename T>
 class NoBinario {
 	public:
-		NoBinario<T>(const T &dado) :										dado(new T(dado)), esquerda(nullptr), direita(nullptr) {}
+		/*!
+		 *  \brief Construtor
+		 *	\param referencia ao tipo generico constante dado do nodo
+		 *
+		 *	Inicializa a arvore, seta o dado do nodo conforme parametro e
+		 *		esquerda e direita sao nulos.
+		 */
+		NoBinario<T>(const T &dado)
+			: dado(new T(dado)), esquerda(nullptr), direita(nullptr) {}
 
+		/*!
+		 *  \brief Destrutor
+		 *
+		 *	Destrutor virtual.
+		 */
 		virtual ~NoBinario() {}
 
+		/*!
+		 *  \brief Funcao getter getDado
+		 *  \return o ponteiro para o dado do nodo.
+		 */
 		T *getDado() {
 			return dado;
 		}
 
+		/*!
+		 *  \brief Funcao getter getElementos
+		 *  \return o vector de nodos binarios elementos.
+		 */
 		std::vector<NoBinario<T>*> getElementos() {
 			return elementos;
 		}
 
+		/*!
+		 *  \brief Funcao getter getEsquerda
+		 *  \return o ponteiro para o nodo filho da esquerda.
+		 */
 		NoBinario<T> *getEsquerda() {
 			return esquerda;
 		}
 
+		/*!
+		 *  \brief Funcao getter getDireita
+		 *  \return o ponteiro para o nodo filho da direita.
+		 */
 		NoBinario<T> *getDireita() {
 			return direita;
 		}
 
+		/*!
+		 *  \brief Funcao setter setEsquerda
+		 *  \param esq passado por nome e um nodo binario.
+		 * 	\return nao possui.
+		 *
+		 *	Atribui o nodo passado como filho da esquerda do nodo pai.
+		 */
 		void setEsquerda(NoBinario<T> *esq) {
 			esquerda = esq;
 		}
 
+		/*!
+		 *  \brief Funcao setter setDireita
+		 *  \param dir passado por nome e um nodo binario.
+		 * 	\return nao possui.
+		 *
+		 *	Atribui o nodo passado como filho da direita do nodo pai.
+		 */
 		void setDireita(NoBinario<T> *dir) {
 			direita = dir;
 		}
 
+		/*!
+		 *  \brief Funcao setter setDado
+		 *  \param data passado por nome e um tipo generico que representa a
+		 *		info do nodo.
+		 * 	\return nao possui.
+		 *
+		 *	Atribui a info passada como a nova informacao do nodo.
+		 */
 		void setDado(T *data) {
 			dado = data;
 		}
 
+		/*!
+		 *  \brief Funcao checkNullptr
+		 *  \param arv passado por nome e um nodo binario
+		 *  \return valor booleano resultante da comparacao
+		 *
+		 *  Checa se o nodo passado e um nullptr.
+		 */
+		bool checkNullptr(NoBinario<T> *arv) {
+			return arv == nullptr ? true : false;
+		}
+
+		/*!
+		 *  \brief Funcao busca
+		 *  \param referencia ao tipo generico constante dado do nodo
+		 *  \param arv passado por nome e um nodo binario que vai ser procurado
+		 *  \return um tipo generico que representa o dado do nodo encontrado
+		 *  \sa getDireita(), getEsquerda(), getDado(), checkNullptr(...)
+		 *
+		 *  Caminha pela arvore a procura do nodo, se ele nao for encontrado,
+		 *		entao lanca uma excecao.
+		 */
 		T *busca(const T &dado, NoBinario<T> *arv) {
-			while (arv != nullptr && dado != *arv->getDado()) {
+			while (!checkNullptr(arv) && dado != *arv->getDado()) {
 				if (*arv->getDado() < dado)
 					arv = arv->getDireita();
 				else
 					arv = arv->getEsquerda();
 			}
 
-			if (arv == nullptr)
+			if (checkNullptr(arv))
 				throw "nodo não encontrado!";
 			else
 				return arv->getDado();
 		}
 
-		NoBinario<T> *buscaNodo(const T &dado, NoBinario<T> *arv) {
-			while (arv != nullptr && dado != *arv->getDado()) {
-				if (*arv->getDado() < dado)
-					arv = arv->getDireita();
-				else
-					arv = arv->getEsquerda();
-			}
-
-			if (arv == nullptr)
-				throw "nodo não encontrado!";
-			else
-				return arv;
-		}
-
+		/*!
+		 *  \brief Funcao inserir
+		 *  \param referencia ao tipo generico constante dado do nodo
+		 *  \param arv passado por nome e um nodo binario que vai ser inserido
+		 *  \return o ponteiro para o nodo binario
+		 *  \sa getDado(), checkNullptr(...), getEsquerda(), getDireita(),
+		 *		setEsquerda(...), setDireita(...)
+		 *
+		 *  Busca de forma recursiva o local correto para inserir o nodo e
+		 *		so realiza o processo de adicao se houver espaco na memoria.
+		 */
 		NoBinario<T> *inserir(const T &dado, NoBinario<T> *arv) {
 			NoBinario<T> *tmpArv;
 
 			if (dado < *arv->getDado()) {  //!< insere a esquerda
-				if (arv->getEsquerda() == nullptr) {
+				if (checkNullptr(arv->getEsquerda())) {
 					tmpArv = new NoBinario<T>(dado);
-					if (semEspaco(tmpArv))
+					if (checkNullptr(tmpArv))
 						throw "sem espaço na memória!";
 					else
 						arv->setEsquerda(tmpArv);
 				} else {
 					arv = inserir(dado, arv->getEsquerda());
 				}
-			} else {                     //!< insere a direita
-				if (arv->getDireita() == nullptr) {
+			} else {                       //!< insere a direita
+				if (checkNullptr(arv->getDireita())) {
 					tmpArv = new NoBinario<T>(dado);
-					if (semEspaco(tmpArv))
+					if (checkNullptr(tmpArv))
 						throw "sem espaço na memória!";
 					else
 						arv->setDireita(tmpArv);
@@ -93,33 +170,11 @@ class NoBinario {
 			}
 		}
 
-		bool semEspaco(NoBinario<T> *arv) {
-			return arv == nullptr ? true : false;
-		}
-
 		NoBinario<T> *remover(NoBinario<T> *arv, const T &dado) {
 			NoBinario<T> *tmpArv;
 			NoBinario<T> *filhoArv;
 
-			/*NoBinario<T> *tmpArv;
-			NoBinario<T> *rmArv = buscaNodo(dado, arv);
-
-			if (rmArv->getEsquerda() == nullptr && rmArv->getDireita() == nullptr) {
-				delete rmArv;
-				return nullptr;
-			} else if (rmArv->getEsquerda() != nullptr) {
-				tmpArv = rmArv;
-				rmArv = rmArv->getEsquerda();
-				delete tmpArv;
-				return rmArv;
-			} else {
-				tmpArv = rmArv;
-				rmArv = rmArv->getDireita();
-				delete tmpArv;
-				return rmArv;
-			}*/
-
-			if (arv == nullptr) {
+			if (checkNullptr(arv)) {
 				return arv;
 			} else {
 				if (dado < *arv->getDado()) {
@@ -130,7 +185,7 @@ class NoBinario {
 						arv->setDireita(remover(arv->getDireita(), dado));
 						return arv;
 					} else { // dois filhos
-						if (arv->getEsquerda() != nullptr							&& arv->getDireita() != nullptr) {
+						if (!checkNullptr(arv->getEsquerda())							&& !checkNullptr(arv->getDireita())) {
 
 							tmpArv = minimo(arv->getDireita());
 							arv->setDado(tmpArv->getDado());
@@ -138,12 +193,12 @@ class NoBinario {
 							return arv;
 						} else { // um filho na direita
 							tmpArv = arv;
-							if (arv->getDireita() != nullptr) {
+							if (!checkNullptr(arv->getDireita())) {
 								filhoArv = arv->getDireita();
 								delete tmpArv;
 								return filhoArv;
 							} else { // um filho na esquerda
-								if (arv->getEsquerda() != nullptr) {
+								if (!checkNullptr(arv->getEsquerda())) {
 									filhoArv = arv->getEsquerda();
 									delete tmpArv;
 									return filhoArv;
@@ -158,30 +213,24 @@ class NoBinario {
 			}
 		}
 
+		/*!
+		 *  \brief Funcao minimo
+		 *  \param ponteiro ao nodo binario que deve ser procurado
+		 *  \return o ponteiro para o nodo minimo da arvore
+		 *  \sa getEsquerda(), checkNullptr(...)
+		 *
+		 *  Percorre a arvore em busca do nodo com o menor dado de forma
+		 *		recursiva.
+		 */
 		NoBinario<T> *minimo(NoBinario<T> *nodo) {
-			bool esquerdaNull = nodo->getEsquerda() == nullptr;
-			bool direitaNull = nodo->getDireita() == nullptr;
-
-			//*************************** versao que pega o nodo com menor dado
-			if (esquerdaNull)
+			if (checkNullptr(nodo->getEsquerda()))
 				return nodo;
 			else
 				return minimo(nodo->getEsquerda());
-
-			/**************************** versao que pega o nodo de maior nivel
-			if (esquerdaNull && direitaNull) {
-				return nodo;
-			} else {
-				if (esquerdaNull)
-					return minimo(nodo->getDireita());
-				else
-					return minimo(nodo->getEsquerda());
-			}
-			******************************************************************/
 		}
 
 		void preOrdem(NoBinario<T> *nodo) {
-			if (nodo != nullptr) {
+			if (!checkNullptr(nodo)) {
 				elementos.push_back(nodo);
 				preOrdem(nodo->getEsquerda());
 				preOrdem(nodo->getDireita());
@@ -189,7 +238,7 @@ class NoBinario {
 		}
 
 		void emOrdem(NoBinario<T> *nodo) {
-			if (nodo != nullptr) {
+			if (!checkNullptr(nodo)) {
 				emOrdem(nodo->getEsquerda());
 				elementos.push_back(nodo);
 				emOrdem(nodo->getDireita());
@@ -197,7 +246,7 @@ class NoBinario {
 		}
 
 		void posOrdem(NoBinario<T> *nodo) {
-			if (nodo != nullptr) {
+			if (!checkNullptr(nodo)) {
 				posOrdem(nodo->getEsquerda());
 				posOrdem(nodo->getDireita());
 				elementos.push_back(nodo);
@@ -205,10 +254,10 @@ class NoBinario {
 		}
 
 	protected:
-		T *dado;
-		NoBinario<T> *esquerda;
-		NoBinario<T> *direita;
+		T *dado;							   //!< informacao generica do nodo
+		NoBinario<T> *esquerda;				   //!< nodo filho da esquerda
+		NoBinario<T> *direita;                 //!< nodo filho da direita
 		std::vector<NoBinario<T>*> elementos;  //!< elementos acessados durante
-											  // o percurso realizado
+											   // o percurso realizado
 };
 #endif
