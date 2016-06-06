c1607b864f98ed8ad8a12b96690c3f28d79b543a
lucasjoao <lucasjoao.lj@gmail.com>
2016-06-05 22:56:33 -0300

    t12: sunday blood sunday

--- TO-DO.md
+++ TO-DO.md
@@ -1,13 +1,22 @@
 dúvidas:
-- retorno do getElementos? Como castear?
+- qual solução seria melhor então?
+    - responder claúdio
+        - https://www.google.com.br/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=why%20inheritance%20is%20bad
+- chaves iguais, deveria ser tratado? antes olhar anotações de ed
 
 binária:
 - implementar destrutor
+    - algo na wiki ou no geek?
 
 avl:
-- implementar
+- implementar como geek for geek e wiki ensinam. Verificar herança
+    - possíveis mudanças se muito problemas:
+        - fazer inserção igual algoritmo do professor
+        - tirar herança
+        - verificar deleção
 - passar nos testes
 - memory leaks
 - organizar código
+- quebrar linhas ou fazer funções para códigos recorrentes
 - comentar
 - enviar
--- t12_arvore_avl/NoAVL.hpp
+++ t12_arvore_avl/NoAVL.hpp
@@ -3,13 +3,14 @@
 
 #include <vector>
 #include <algorithm>
+#include <cmath>
 #include "../t11_arvore_binaria_busca/NoBinario.hpp"
 
 template <typename T>
 class NoAVL : private NoBinario<T> {
 	public:
 		explicit NoAVL(const T &dado)
-			: NoBinario<T>::NoBinario<T>(dado), altura(0) {}
+			: NoBinario<T>::NoBinario(dado), altura(0) {}
 
 		virtual ~NoAVL() { }
 
@@ -29,8 +30,10 @@ class NoAVL : private NoBinario<T> {
 		 *  \brief Funcao getter getElementos
 		 *  \return o vector de nodos binarios elementos.
 		 */
-		std::vector<NoAVL<T>*> getElementos() {
-			return NoBinario<T>::getElementos();
+
+		// VERIFICAR ISSO AQUI
+		void getElementos() {
+			NoBinario<T>::getElementos();
 		}
 
 		/*!
@@ -61,17 +64,117 @@ class NoAVL : private NoBinario<T> {
 			altura = alt;
 		}
 
-		int alturaNo(NoAVL<T> *nodo) {
-			return nodo == nullptr ? -1 : nodo->getAltura();
-		}
-
 		T *busca(const T &dado, NoAVL<T> *arv) {
 			return NoBinario<T>::busca(dado, arv);
 		}
 
-		NoAVL<T> *inserir(const T &dado, NoAVL<T> *arv) { }
+		bool checkNullptr(NoBinario<T> *arv) {
+			return arv == nullptr ? true : false;
+		}
 
-		NoAVL<T> *remover(NoAVL<T> *arv, const T &dado) { }
+		NoAVL<T> *inserir(const T &dado, NoAVL<T> *arv) {
+			NoAVL<T> *tmpArv;
+
+			if (dado < *arv->getDado()) {  //!< insere a esquerda
+				if (checkNullptr(arv->getEsquerda())) {
+					tmpArv = new NoAVL<T>(dado);
+					if (checkNullptr(tmpArv))
+						throw "sem espaço na memória!";
+					else
+						arv->setEsquerda(tmpArv);
+				} else {
+					arv = inserir(dado, arv->getEsquerda());
+
+					if (abs((alturaNo(arv->getEsquerda()) - alturaNo(arv->getDireita()))) > 1) {
+						if (arv->getEsquerda()->getEsquerda() == nullptr)
+							arv = rotacaoEsqDupla(arv);
+						else
+							arv = rotacaoEsqSimples(arv);
+					} // else {
+						// arv->setAltura(std::max(alturaNo(arv->getEsquerda()), alturaNo(arv->getDireita())) + 1);
+					// }
+					// MELHORAR ESSA PARTE!!!
+				}
+			} else {                       //!< insere a direita
+				if (checkNullptr(arv->getDireita())) {
+					tmpArv = new NoAVL<T>(dado);
+					if (checkNullptr(tmpArv))
+						throw "sem espaço na memória!";
+					else
+						arv->setDireita(tmpArv);
+				} else {
+					arv = inserir(dado, arv->getDireita());
+
+					if (abs((alturaNo(arv->getEsquerda()) - alturaNo(arv->getDireita()))) > 1) {
+						if (arv->getDireita()->getDireita() == nullptr)
+							arv = rotacaoDirDupla(arv);
+						else
+							arv = rotacaoDirSimples(arv);
+					} // else {
+						// arv->setAltura(std::max(alturaNo(arv->getEsquerda()), alturaNo(arv->getDireita())) + 1);
+					// }
+					// MELHORAR ESSA PARTE!!!
+				}
+			}
+
+			// MELHORAR ESSA PARTE!!!
+			arv->setAltura(std::max(alturaNo(arv->getEsquerda()), alturaNo(arv->getDireita())) + 1);
+			return arv;
+		}
+
+		NoAVL<T> *remover(NoAVL<T> *arv, const T &dado) {
+			NoAVL<T> *tmpArv = arv;
+			NoAVL<T> *filhoArv;
+
+			if (dado < *arv->getDado()) {
+				/* busca do nodo a ser removido */
+				arv->setEsquerda(remover(arv->getEsquerda(), dado));
+			} else if (*arv->getDado() < dado) {
+				/* busca do nodo a ser removido */
+				arv->setDireita(remover(arv->getDireita(), dado));
+			} else if (!checkNullptr(arv->getEsquerda())
+						&& !checkNullptr(arv->getDireita())) {
+				/* dois filhos */
+				tmpArv = minimo(arv->getDireita());
+				arv->setDado(tmpArv->getDado());
+				arv->setDireita(remover(arv->getDireita(), *arv->getDado()));
+			} else if (!checkNullptr(arv->getDireita())) {
+				/* filho na direita */
+				filhoArv = arv->getDireita();
+				delete tmpArv;
+				return filhoArv;
+			} else if (!checkNullptr(arv->getEsquerda())) {
+				/* filho na esquerda */
+				filhoArv = arv->getEsquerda();
+				delete tmpArv;
+				return filhoArv;
+			} else {
+				/* sem filhos */
+				delete tmpArv;
+				return nullptr;
+			}
+
+			// arv->setAltura(std::max(alturaNo(arv->getEsquerda()), alturaNo(arv->getDireita())) + 1);
+
+			// errado, não serve para todos -- arrumar!
+			if (abs((alturaNo(arv->getEsquerda()) - alturaNo(arv->getDireita()))) > 1) {
+				if (arv->getDireita() == nullptr) {
+					if (arv->getEsquerda()->getEsquerda() == nullptr)
+						arv = rotacaoEsqDupla(arv);
+					else
+						arv = rotacaoEsqSimples(arv);
+				} else if (arv->getEsquerda() == nullptr) {
+					if (arv->getDireita()->getDireita() == nullptr)
+						arv = rotacaoDirDupla(arv);
+					else
+						arv = rotacaoDirSimples(arv);
+				}
+			}
+
+			arv->setAltura(std::max(alturaNo(arv->getEsquerda()), alturaNo(arv->getDireita())) + 1);
+
+			return arv;
+		}
 
 		NoAVL<T> *minimo(NoAVL<T> *nodo) {
 			return (NoAVL<T> *) NoBinario<T>::minimo(nodo);
@@ -86,7 +189,11 @@ class NoAVL : private NoBinario<T> {
 		}
 
 		void posOrdem(NoAVL<T> *nodo) {
-			NoBinario<T>::preordem(nodo);
+			NoBinario<T>::preOrdem(nodo);
+		}
+
+		int alturaNo(NoAVL<T> *nodo) {
+			return nodo == nullptr ? -1 : nodo->getAltura();
 		}
 
 		NoAVL<T> *rotacaoEsqSimples(NoAVL<T> *arv) {
@@ -98,8 +205,8 @@ class NoAVL : private NoBinario<T> {
 				//
 				// DA DE QUEBRAR ESSAS LINHAS?
 				//
-				arv->setAltura(max(alturaNo(arv->getEsquerda()), alturaNo(arv->getDireita())) + 1);
-				arvTmp->setAltura(max(alturaNo(arvTmp->getEsquerda()), alturaNo(arvTmp->getDireita())) + 1);
+				arv->setAltura(std::max(alturaNo(arv->getEsquerda()), alturaNo(arv->getDireita())) + 1);
+				arvTmp->setAltura(std::max(alturaNo(arvTmp->getEsquerda()), alturaNo(arvTmp->getDireita())) + 1);
 
 				return arvTmp;
 			} else {
@@ -116,10 +223,34 @@ class NoAVL : private NoBinario<T> {
 				//
 				// DA DE QUEBRAR ESSAS LINHAS?
 				//
-				arv->setAltura(max(alturaNo(arv->getEsquerda()), alturaNo(arv->getDireita())) + 1);
-				arvTmp->setAltura(max(alturaNo(arvTmp->getEsquerda()), alturaNo(arvTmp->getDireita())) + 1);
+				arv->setAltura(std::max(alturaNo(arv->getEsquerda()), alturaNo(arv->getDireita())) + 1);
+				arvTmp->setAltura(std::max(alturaNo(arvTmp->getEsquerda()), alturaNo(arvTmp->getDireita())) + 1);
 
 				return arvTmp;
+			} else {
+				throw "ERRO!";
+			}
+		}
+
+		NoAVL<T> *rotacaoEsqDupla(NoAVL<T> *arv) {
+			//
+			// DA DE QUEBRAR ESSAS LINHAS?
+			//
+			if (arv->getEsquerda() != nullptr && arv->getEsquerda()->getDireita() != nullptr) {
+				arv->setEsquerda(rotacaoDirSimples(arv->getEsquerda()));
+				return rotacaoEsqSimples(arv);
+			} else {
+				throw "ERRO!";
+			}
+		}
+
+		NoAVL<T> *rotacaoDirDupla(NoAVL<T> *arv) {
+			//
+			// DA DE QUEBRAR ESSAS LINHAS?
+			//
+			if (arv->getDireita() != nullptr && arv->getDireita()->getEsquerda() != nullptr) {
+				arv->setDireita(rotacaoEsqSimples(arv->getDireita()));
+				return rotacaoDirSimples(arv);
 			} else {
 				throw "ERRO!";
 			}
